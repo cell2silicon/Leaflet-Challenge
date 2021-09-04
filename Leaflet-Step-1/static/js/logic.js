fcaef122af0bd30040d254c711985da926737068
cell2silicon <83265414+cell2silicon@users.noreply.github.com>
2021-09-04 22:37:40 +1000

    Read.me

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -5,14 +5,15 @@ var earthquakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/a
 var tectonicUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
 
-// Perform a GET request to the query URL
+// Perform a GET request to the query Earthquake URL
 d3.json(earthquakeUrl).then(function(earthData) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  console.log(earthData);
+  // console.log(earthData);
 });
 
+// Perform a GET request to the query Tectonic Plate URL
 d3.json(tectonicUrl).then(function(plateData){
-  console.log(plateData);
+  // console.log(plateData);
+  // Add techtonic plate data to map
   L.geoJSON(plateData, {
     color: "#7fc97f",
     weight: 3
@@ -22,7 +23,7 @@ d3.json(tectonicUrl).then(function(plateData){
 });
 
 
-  // Define streetmap and darkmap layers
+  // Define streetmap, darkmap and satellite map layers
   var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -31,8 +32,6 @@ d3.json(tectonicUrl).then(function(plateData){
     id: "mapbox/streets-v11",
     accessToken: API_KEY
   });
-
-  
 
   var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
